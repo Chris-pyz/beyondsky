79e87adac2ce4485f964441b97f60d244a8713ea
anais meirone <anais.meirone@live.fr>
2021-08-18 12:17:44 +0200

    add dashboard and some links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :spaceships
+  resources :users, only: [:show]
 
   resources :bookings, only: %i[index show new create edit update]
 end
