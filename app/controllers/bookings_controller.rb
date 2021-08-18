62cf689b45f9e2fd3610b7999754b04754200597
Genevieve Michaud <migenevieve@gmail.com>
2021-08-18 12:19:07 +0200

    controllerBookingMethodeNew-show-create-index + nested resources

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,39 @@
 class BookingsController < ApplicationController
+
+  # /spaceships/:spaceship_id/bookings
+  def index
+    @bookings = Booking.all
+  end
+
+  # spaceships/:spaceship_id/bookings/:id
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  # /spaceships/:id/bookings/new
+  def new
+    @booking = Booking.new
+    @spaceship = Spaceship.find(params[:spaceship_id])
+  end
+
+  def create
+    @spaceship = Spaceship.find(params[:spaceship_id])
+    @booking = Booking.new(booking_params)
+    @booking.spaceship = @spaceship
+    @booking.user = current_user
+
+    if @booking.save
+
+      redirect_to @spaceship, notice: 'booking was successfully created.'
+    else
+      render :new
+
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
