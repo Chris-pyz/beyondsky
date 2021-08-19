c2ea0d25ab8bc9b9ba75ca99a01ae5dd6f1f708a
Genevieve Michaud <migenevieve@gmail.com>
2021-08-19 12:18:33 +0200

    restart terminal prise 2

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -4,4 +4,5 @@ class Spaceship < ApplicationRecord
   # has_many :reviews, through: :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many_attached :photos
 end
